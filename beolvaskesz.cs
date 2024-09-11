9b63f8330a6744f293aa641bab69e27e3c286e9b
lacko186 <laszlobogdan0619@gmail.com>
2024-09-11 19:19:06 +0200

    gyakorlo

---
+++ beolvaskesz.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Beolvas
+{
+	public class Program
+	{
+    		public static void Main(string[] args)
+    		{
+        	System.Console.WriteLine("Hello, World!");
+    		}
+	}
+}
